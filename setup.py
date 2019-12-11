114bdbaccbd25b1bdb8a85a376f4242444f6d732
Paul Hammant <paul@hammant.org>
2019-12-11 11:07:45 +0000

    packge rename

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='servirtium-python',
+setup(name='servirtium',
       version='0.1',
-      description='Python Servirtium port',
-      author='',
-      author_email='',
-      url='https://github.com/servirtium',
+      description='Servirtium service virtualization',
+      author='Paul Hammant',
+      author_email='paul@hammant.org',
+      url='https://github.com/servirtium/servirtium-python',
       packages=['src'],
      )
