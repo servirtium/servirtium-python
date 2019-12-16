7c6e777df56778532d0f7e84f4934a21fb64c5fa
Paul Hammant <paul@hammant.org>
2019-12-16 14:20:28 +0000

    copyright year

--- servirtium/markdown_parser.py
+++ servirtium/markdown_parser.py
@@ -1,6 +1,6 @@
 #        Servirtium: Service Virtualized HTTP
 #
-#        Copyright (c) 2018, Paul Hammant and committers
+#        Copyright (c) 2019, Paul Hammant and committers
 #        All rights reserved.
 #
 #        Redistribution and use in source and binary forms, with or without
