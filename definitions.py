eb756a56b36f55348cc732aed005d06a299996d6
Paul Hammant <paul@hammant.org>
2019-12-18 15:27:09 +0000

    inching towards unit test

--- definitions.py
+++ definitions.py
@@ -1,4 +1,3 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-MOCKS_DIR = ROOT_DIR + "/src/mocks/"
--- test/test_markdown_parser.py
+++ test/test_markdown_parser.py
@@ -1,25 +1,12 @@
 import json
-import os
 import pytest
 from unittest.mock import patch
 
-from servirtium.markdown_parser import SimpleMarkdownParser
+from markdown_parser import SimpleMarkdownParser
 
 
-class TestMarkdownParser:
-
-    if __name__ == "__main__":
-        try:
-            pytest.main(["-x", os.getcwd()+"/test_markdown_parser.py"])
-        finally:
-            os._exit(1)
-
-    def test_something(self):
-
-        @patch('SimpleMarkdownParser.get_file_content')
-        def test(mock_get_file_content):
-            mock_get_file_content.return_value = 'abc 123'
-            recording = SimpleMarkdownParser().get_recording_from_method_name("foo")
-            print(json.dumps(recording))
-            pytest.fail("oops")
-
+@patch('markdown_parser.SimpleMarkdownParser.get_file_content')
+def test_bad_mrkdown_means_no_recordings(mock_get_file_content):
+    mock_get_file_content.return_value = 'abc 123'
+    recording = SimpleMarkdownParser().get_recording_from_method_name("foo")
+    assert recording is None
