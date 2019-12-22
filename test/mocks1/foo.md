bed2806b1c9c257f7958f52d914a8eb2b0530e06
Paul Hammant <paul@hammant.org>
2019-12-22 14:47:42 +0000

    parser tests

---
+++ test/mocks1/foo.md
@@ -0,0 +1,54 @@
+## Interaction 0: GET /path/to/resource
+
+### Request headers recorded for playback:
+
+```
+Header1: 11
+Header2: 22
+```
+
+### Request body recorded for playback (application/xml):
+
+```
+abc123
+```
+
+### Response headers recorded for playback:
+
+```
+Content-Type: text/plain
+Header-X: abc-123
+```
+
+### Response body recorded for playback (200: text/plain):
+
+```
+Mary had a little lamb
+```
+
+## Interaction 1: POST /path/to/another/thing
+
+### Request headers recorded for playback:
+
+```
+Its_fleece: Was white as snow
+```
+
+### Request body recorded for playback (mime/type):
+
+```
+And every where that Mary went
+```
+
+### Response headers recorded for playback:
+
+```
+Content-Type: nursery/rhyme
+The_lamb: was sure to go ;
+```
+
+### Response body recorded for playback (200: mime/type):
+
+```
+He followed her to school one day—
+```
--- test/test_markdown_parser.py
+++ test/test_markdown_parser.py
@@ -6,7 +6,35 @@ from markdown_parser import SimpleMarkdownParser
 
 
 @patch('markdown_parser.SimpleMarkdownParser.get_file_content')
-def test_bad_mrkdown_means_no_recordings(mock_get_file_content):
+def test_bad_markdown_means_no_recordings(mock_get_file_content):
     mock_get_file_content.return_value = 'abc 123'
     recording = SimpleMarkdownParser().get_recording_from_method_name("foo")
-    assert recording is None
+    assert recording is None\
+
+def test_good_markdown_means_recording():
+    parser = SimpleMarkdownParser()
+    files = parser.get_markdown_file_strings("./test/mocks1")
+    parser._set_mock_files(files)
+    recording = parser.get_recording_from_method_name("foo")
+    assert recording is not None
+    assert len(recording.interactions) is 2
+
+    i = recording.interactions[0]
+    assert i.path == "/path/to/resource"
+    assert str(i.request_headers) == "{'Header1': '11', 'Header2': '22'}"
+    assert i.request_body == "abc123"
+
+    assert i.response_type == "text/plain"
+    assert i.response_code == ""
+    assert str(i.response_headers) == "{'Content-Type': 'text/plain', 'Header-X': 'abc-123'}"
+    assert i.response_body == "Mary had a little lamb"
+
+    i = recording.interactions[1]
+    assert i.path == "/path/to/another/thing"
+    assert str(i.request_headers) == "{'Its_fleece': 'Was white as snow'}"
+    assert i.request_body == "And every where that Mary went"
+
+    assert i.response_type == "nursery/rhyme"
+    assert i.response_code == ""
+    assert str(i.response_headers) == "{'Content-Type': 'nursery/rhyme', 'The_lamb': 'was sure to go ;'}"
+    assert i.response_body == "He followed her to school one day—"
