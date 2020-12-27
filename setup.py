be73f05a319b31455cd93fdfc9d7f9d436870a2b
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-12-27 11:21:15 +0530

    Updated setup.py to make release

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='servirtium',
-      version='0.1',
-      description='Servirtium service virtualization',
-      author='Paul Hammant',
-      author_email='paul@hammant.org',
-      url='https://github.com/servirtium/servirtium-python',
-      packages=['servirtium'], requires=['requests', 'pytest']
-      )
+setup(
+    name='servirtium',
+    version='0.1.0',
+    description='Servirtium service virtualization',
+    author='Paul Hammant',
+    author_email='paul@hammant.org',
+    url='https://github.com/servirtium/servirtium-python',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=find_packages(exclude="test"),
+    requires=['requests', 'pytest']
+)
